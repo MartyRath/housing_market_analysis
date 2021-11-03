cbdbd62e2f540a9a557667f98771b20f8bd81ea6
MartyRath <martydavidger@gmail.com>
2021-11-03 17:14:19 +0000

    FGINAL FINAL TEST

--- Importing data.py
+++ Importing data.py
@@ -1,3 +1,7 @@
 import pandas as pd
 x = pd.read_csv(r'C:\Users\User\Desktop\UCD DATA\Property Price Register Datasets\Property_Price_Register_Ireland-28-05-2021.csv')
-print(x.head())
+#print(x.head())
+
+df=pd.DataFrame(x)
+#print(df.head())
+print(df.columns)
