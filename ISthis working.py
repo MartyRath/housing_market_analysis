6c40f431e464a0f3f020f70e8610e3cf2074dfdb
MartyRath <martydavidger@gmail.com>
2021-11-03 17:09:38 +0000

    FGINAL FINAL TEST

---
+++ ISthis working.py
@@ -0,0 +1,5 @@
+#OK final test
+print('a')
+import pandas as pd
+x = pd.read_csv(r'C:\Users\User\Desktop\UCD DATA\Property Price Register Datasets\Property_Price_Register_Ireland-28-05-2021.csv')
+print(x.head())
