84eaeae16e628c37909fc1b1ac5b0a3fa9c11a93
MartyRath <martydavidger@gmail.com>
2021-11-07 13:06:27 +0000

    Added more

--- ISthis working.py
+++ ISthis working.py
@@ -1,2 +1,14 @@
-df = pd.DataFrame(file)
+#df = pd.DataFrame(file)
 
+import requests
+from bs4 import BeautifulSoup
+r=requests.get('https://www.daft.ie/property-for-sale/ireland/houses')
+
+html_doc=r.text
+soup=BeautifulSoup(html_doc, features="html.parser")
+#print(soup)
+print(soup.title.text)
+
+#print(soup.prettify())
+#print(request.status_code)
+#print(request.text)
