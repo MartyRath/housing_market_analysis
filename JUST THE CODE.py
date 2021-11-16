dc67f805217991f5ddcd9c605f6718fc2c1b3777
MartyRath <martydavidger@gmail.com>
2021-11-16 18:09:36 +0000

    SCATTER

--- JUST THE CODE.py
+++ JUST THE CODE.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 df = pd.read_csv(r'C:\Users\User\Desktop\UCD DATA\Test\Property_Price_Register_Ireland-28-05-2021.csv')
 pd.options.display.width= None
 pd.options.display.max_columns= None
@@ -8,4 +10,31 @@ df.drop(['PROPERTY_SIZE_DESC'],axis=1,inplace=True)
 df['PROPERTY_DESC'].replace({'Teach/�ras�n C�naithe Nua': 'New Dwelling house /Apartment', 'Teach/?ras?n C?naithe Nua': 'New Dwelling house /Apartment', 'Teach/�ras�n C�naithe Ath�imhe': 'Second-Hand Dwelling house /Apartment'}, inplace=True)
 df['SALE_DATE'] = pd.to_datetime(df['SALE_DATE'])
 df['YEAR'], df['MONTH'] = df['SALE_DATE'].dt.year , df['SALE_DATE'].dt.month
-print(df.head())
+SALE_PRICE_STATS = df.groupby('YEAR')['SALE_PRICE'].agg([np.min, np.max, np.mean, np.median])
+
+#print(df.head())
+df2020= df[df['YEAR'] == 2020]
+#print(df2020.head())
+
+mean = SALE_PRICE_STATS['mean']
+for unique_values in df:
+    year=(df['YEAR'].unique())
+
+plt.scatter(year, mean)
+plt.xlabel('Year')
+plt.ylabel('Sale Price (€)')
+plt.title('Average Sale Price Per Year')
+plt.yticks([200000, 220000, 240000, 260000, 280000, 300000, 320000], ['200K', '220K', '240K', '260K', '280K', '300K', '320K'])
+plt.show()
+
+#HAHA = df2020.groupby('MONTH')['SALE_PRICE'].agg(np.mean)
+#print(HAHA)
+#plt.plot(HAHA)
+#plt.show()
+
+#COUNTY PRICE STATS
+#COUNTY_PRICE_STATS = df2020.groupby('COUNTY')['SALE_PRICE'].agg([np.min, np.max, np.mean, np.median])
+#somean= COUNTY_PRICE_STATS['mean'].tolist()
+#print(somean)
+#plt.hist(somean, bins=32)
+#plt.show()
