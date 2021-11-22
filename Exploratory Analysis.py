3384e3712652a2027429585ff8b238f326f5c9a2
MartyRath <martydavidger@gmail.com>
2021-11-22 19:26:46 +0000

    CLEANING UP more

---
+++ Exploratory Analysis.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+Leinster = pd.read_csv(r'C:\Users\User\Desktop\UCD DATA\Leinster_PPR_2010-2020.csv')
+inflation = pd.read_csv(r'C:\Users\User\Desktop\UCD DATA\Inflation.csv')
+Leinster_budget=Leinster[Leinster['SALE_PRICE']<110000]
+Leinster2020= Leinster[Leinster['YEAR'] == 2020]
+
+SALE_PRICE_STATS = Leinster2020.groupby('YEAR')['SALE_PRICE'].agg([np.min, np.max, np.mean, np.median])
+print(SALE_PRICE_STATS)
+
+# Are second hand houses much cheaper?
+oldnew = Leinster.groupby('PROPERTY_DESC')['SALE_PRICE'].mean() # not much difference
+#print(oldnew)
+
+# Cheapest houses sold in 2020 # 6500 cheapest
+Leinster2020.sort_values('SALE_PRICE')
+
+# How many houses sold for under 110K in 2020 # 1845
+Leinster2020[Leinster2020['SALE_PRICE']<110000]
+
+# Cheapest houses in 2020 by county=Wexford # 6500 cheapest
+Wexford= Leinster2020[Leinster2020['COUNTY']=='Wexford']
+Wexford['SALE_PRICE'].min()
+
+# How many houses sold under 110K in Wexford 2020 # 296
+Leinster2020[(Leinster2020['COUNTY']=='Wexford') & (Leinster2020['SALE_PRICE']<110000)]
+
+# Custom code using IQR, instead of standard deviation, as there's outliers property prices. This shows where most of the data lies.
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+IQR2020 = Leinster2020['SALE_PRICE'].agg(iqr)
+#print(IQR2020) # 196500.0
